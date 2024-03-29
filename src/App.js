f052cbc44bf0e140cab2864f9512b4237e8ac83c
Abhishek kumar <aasalex21c@gmail.com>
2024-03-29 15:09:24 +0530

    almost done, omg, my most difficult react project so far...

--- src/App.js
+++ src/App.js
@@ -1,57 +1,113 @@
-import { useState } from 'react';
 import './App.css';
 import './Assests/fontAwesomeProIcons/fontAwesomeIcons.css';
-import { useEffect } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import SuccessAndErrorMsg from './Components/SuccessAndErrorMsg';
 import QuestionDialog from './Components/QuestionDialog';
 
-function App() {
 
+function App() {
+  function reducer(state, action){
+    if(action.type === 'clearInterval'){
+      // console.log(state);
+      clearTimeout(state.timeoutId);
+      clearInterval(state.intervalId);
+      return {
+        intervalId : null,
+        timeoutId : null
+      }
+    }else if(action.type === 'setIntervalId'){
+      return {
+        ...state, 
+        intervalId:  action.intervalId
+      };
+    }else if(action.type === 'setTimeoutId'){
+      return {
+        ...state, 
+        timeoutId:  action.timeoutId
+      };
+    }else{
+      return {...state};
+    }
+  }
+   
+  let [stateIntervalId, dispatch] = useReducer(reducer, null);
+  let currentQuestionNo = 1;
   function beginCountdown(){
-    console.log(stateQuizWebApp.countdownProhibitedToRun, stateQuizWebApp.questions.length);
     if(stateQuizWebApp.countdownProhibitedToRun){
-      console.log('returning from beginCoutdown!');
+      //console.log('returning from beginCoutdown!');
       return;
     }
-    let intervalId;
-    intervalId = setInterval(()=>{
-      // decrease the timeout 
-        updateStateTimeLeft(previousState=>{
-          return previousState-1000
-        });
-    }, 1000);
+    
+   
+    askQuestions();
+    function askQuestions(){
+      let intervalId = (
+          setInterval(()=>{
+          // decrease the timeout 
+            updateStateTimeLeft(previousState=>{
+              return previousState-1000
+            });
+        }, 1000)
+      );
+      dispatch({type: 'setIntervalId', intervalId : intervalId});
+      
+      
+  
+      let timeoutId = setTimeout(() => {      
+        // clearing the interval       
+          dispatch({type: 'clearInterval'});
 
+        // do not execute this codeblock, if user have pressed button skip Question or have clicked on any answer
+        // so i'm using clearTimeout for this
+        // now i want to switch to next question
 
-    setTimeout(() => {      
-      // clearing the interval       
-      clearInterval(intervalId);      
-      // now i want to switch to next question
-        updateStateQuizWebApp(previousState=>{
-          return {
-            ...previousState,
-            currentQuestionNo: previousState.currentQuestionNo+1,
-            countdownProhibitedToRun : (previousState.currentQuestionNo+1 >= previousState.questions.length) ? true : false
+        currentQuestionNo++;
+          
+          if(currentQuestionNo <=10){
+            // keep asking questions
+              updateStateQuizWebApp(previousState=>{
+                // //console.log(`${previousState.currentQuestionNo+1} >= 10`, (previousState.currentQuestionNo+1 >= 10) ? true : false)
+                return {
+                  ...previousState,
+                  currentQuestionNo: previousState.currentQuestionNo +1,
+                  countdownProhibitedToRun : previousState.currentQuestionNo+1 > previousState.questions.length ? true : false
+                }
+              });            
+              updateStateTimeLeft(stateQuizWebApp.timeout);
+              askQuestions();
+
+          }else{
+            console.log('marking flag true allQuestionsAsked!');
+            updateStateQuizWebApp(previousState=>{
+              return {
+                ...previousState,
+                allQuestionsAsked: true
+              }
+
+            });
           }
-        });
-      // re-initialize
-       updateStateTimeLeft(stateQuizWebApp.timeout);
-      // and begin countdown
-      if(!stateQuizWebApp.countdownProhibitedToRun){
-        beginCountdown();
-      }
+  
+  
+      }, stateQuizWebApp.timeout);
 
-    }, stateQuizWebApp.timeout)
+      dispatch({type: 'setTimeoutId', timeoutId : timeoutId});
+
+    }
   }
   function skipQuestion(){
     // update currentquestion no      
     // record user response as nothing or accordingly    
+    dispatch({type: 'clearInterval'});
     updateStateQuizWebApp(previousState=>{
       return {
         ...previousState,
+        allQuestionsAsked: previousState.currentQuestionNo+1 >= previousState.questions.length ? true : false,
         currentQuestionNo: previousState.currentQuestionNo+1,
-        countdownProhibitedToRun : previousState.currentQuestionNo+1 >= previousState.questions.length ? true : false
+        countdownProhibitedToRun : previousState.currentQuestionNo>= previousState.questions.length ? true : false,
       }
     });
+    updateStateTimeLeft(stateQuizWebApp.timeout);
+    beginCountdown();
 
   }
   
@@ -59,7 +115,7 @@ function App() {
     try {
       let response = await fetch(stateQuizWebApp.apiURL);
       let result = await response.json();
-      // console.log(result.results);
+      // //console.log(result.results);
       // update the state with the fetched data
       result = [...result.results];
 
@@ -69,30 +125,62 @@ function App() {
           questions: [...result]
         }
       });
+      beginCountdown();
+
     } catch (error) {
       updateStateQuizWebApp(previousState=>{
         return {
           ...previousState,
-          countdownProhibitedToRun: true,
+          countdownProhibitedToRun: true,   
+          criticalErrorOccurred: true,       
           SuccessAndErrorMsg:{
             ...previousState.SuccessAndErrorMsg,            
             msgType:"Error",
-            msg: `Failed to fetch questions from API. ${error}`,
+            msg: `Please try again after some time, Failed to fetch questions from API. ${error}`,
             displayNone: ''  
           }
         }
       });
-      // console.log("ERROR: Failed to fetch questions from API." + error);
-    } finally{
-      beginCountdown();
+      // //console.log("ERROR: Failed to fetch questions from API." + error);
     }
   }
+  function handleUserAnswers(userAnsweredCorrectly){
+    //console.log('inside', userAnsweredCorrectly);
+
+    // now i want to update the no of correct answers users had made
+    // and also want to ask next question
+    dispatch({type: 'clearInterval'});
+    // alert(intervalId);
+    updateStateTimeLeft(stateQuizWebApp.timeout);
+
+    updateStateQuizWebApp(previousState=>{
+      return {
+        ...previousState,
+        currentQuestionNo: previousState.currentQuestionNo+1,
+        allQuestionsAsked: previousState.currentQuestionNo >= previousState.questions.length ? true : false,
+        userResponse : {
+          ...previousState.userResponse,
+          correctAnswers: userAnsweredCorrectly ? previousState.userResponse.correctAnswers + 1 : previousState.userResponse.correctAnswers
+        }
+      }
+    });  
+
+    beginCountdown();
+  }
+
+
+
   function returnInitialState(){
     return {
       apiURL : "https://opentdb.com/api.php?category=18&amount=10&difficulty=easy&type=multiple",
-      timeout: 2000, // seconds
+      timeout: 7000, // seconds
       countdownProhibitedToRun: false,
+      criticalErrorOccurred: false,
+      allQuestionsAsked : false,
       intervalID: null,
+      userResponse: {
+        correctAnswers: 0
+      },
       questions : [{
           "type": "multiple",
           "difficulty": "easy",
@@ -119,11 +207,14 @@ function App() {
   }
   let [stateQuizWebApp, updateStateQuizWebApp] = useState(returnInitialState());
   let [stateTimeLeft, updateStateTimeLeft] = useState(stateQuizWebApp.timeout);
+  // useEffect(()=>{
+  //   //console.log(stateQuizWebApp);
+  // }, [stateQuizWebApp]);
 
   useEffect(()=>{
-    // console.log(stateQuizWebApp.apiURL);
+    // //console.log(stateQuizWebApp.apiURL);
     // making an API Call
-    console.log('making api call');
+    //console.log('making api call');
     makeAPICall();
     
 
@@ -131,36 +222,56 @@ function App() {
 
 
 
+
     
     return (      
-      <div className="wrapperLocalTaskerApp border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">        
+      <div className="wrapperQuizWebApp border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">        
         <h1 className='text-blue-300 text-[2.5rem] font-semibold smallCaps flex flex-col gap-[0rem]  text-center'><span>Quiz WebApp</span> <span className='text-[1.5rem]'>( <a href="https://geekster.in/" className='underline hover:text-yellow-300 transition'>Geekster</a> Module#5 React Day #9 Homework :)</span></h1>
         <SuccessAndErrorMsg  stateSuccessAndErrorMsg={stateQuizWebApp.SuccessAndErrorMsg}/>
         {
-        stateQuizWebApp.currentQuestionNo <=10 ? 
+        stateQuizWebApp.currentQuestionNo <= stateQuizWebApp.questions.length && !stateQuizWebApp.allQuestionsAsked && !stateQuizWebApp.criticalErrorOccurred && 
         <QuestionDialog 
           currentQuestionNo={stateQuizWebApp.currentQuestionNo} 
           question={stateQuizWebApp.questions[stateQuizWebApp.currentQuestionNo-1]}
+          handleUserAnswers= {handleUserAnswers}
         />
-        : 
-        // Result Card
-          <div>
-
-          </div>
         }
-        {/* Time Left */}
+
         {
+          stateQuizWebApp.allQuestionsAsked  ?
           <div>
-            <span>Time Left: </span>
-            <span>{stateTimeLeft/1000} Seconds</span>
-          </div>          
+            <h2 className='text-[1.8rem] font-semibold'>Quiz Ended</h2>
+            <p>
+              Your Score: {Number(stateQuizWebApp.userResponse.correctAnswers)}/{stateQuizWebApp.questions.length}
+            </p>
+          </div>
+        
+            :
+          
+              
+              !stateQuizWebApp.countdownProhibitedToRun &&
+              <div>
+                <div className='flex gap-[1rem] items-center'>
+                  <span className='flex gap-[.5rem] items-center'>
+                    <i className="fa-solid fa-hourglass-half text-[2rem] text-blue-300"></i>
+                    <span>Time Left:</span>
+                  </span>
+                  <span>{stateTimeLeft/1000} Seconds</span>
+                </div>   
+                
+                <button onClick={skipQuestion} className="font-semibold flex gap-[1rem] items-center justify-center outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer mt-[1rem] py-[.5rem] p-[1rem] rounded-md hover:text-slate-50 text-slate-900 text-[1.5rem] select-none" type="submit">
+                <i className="fa-solid fa-forward"></i>
+                <span>Skip Question</span>
+                </button>
+            </div>
+          
+       
+              
+
+
         }
     
 
-        <button onClick={skipQuestion} className="font-semibold flex gap-[1rem] items-center justify-center outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer p-[1rem] rounded-md hover:text-slate-50 text-slate-900 text-[1.5rem]" type="submit">
-          <i className="fa-solid fa-forward"></i>
-          <span>Skip Question</span>
-          </button>
       </div>
   
     );
--- src/Components/QuestionDialog.jsx
+++ src/Components/QuestionDialog.jsx
@@ -1,18 +1,52 @@
 import he from 'he'; // Import the he library for decoding html entities like single quotes as fetched from api
+import { useRef } from 'react';
 
-export default function QuestionDialog({currentQuestionNo, question}){
-  console.log(question);
+
+export default function QuestionDialog({currentQuestionNo, question, handleUserAnswers}){
+  let radioRefs = useRef([]);
+
+  function resetCheckboxState() {
+    // Loop through each radio input and reset its checked property
+    radioRefs.current.forEach(radio => {
+      radio.checked = false;
+    });
+  
+  }
+
+  function doesUserAnsweredCorrectly(event){
+    let userAnswer = event.target.value;
+    event.target.checked = true;
+    // //console.log(question.incorrect_answers, question.incorrect_answers.includes(userAnswer));
+    let userAnsweredCorrectly = false;
+    if(!question.incorrect_answers.includes(userAnswer)){
+      // that means user has answered the question correctly
+        userAnsweredCorrectly = true;
+    }
+    // //console.log(userAnswer);
+    // handleUserAnswers
+    
+    setTimeout(()=>{
+      resetCheckboxState();
+      handleUserAnswers(userAnsweredCorrectly);
+    }, 200);
+    
+
+  }
+
+  // //console.log(question);
   let options = [...question.incorrect_answers, question.correct_answer];
   return (
     <section className='flex flex-col gap-[.5rem]'>
       <h2 className='text-[1.8rem] font-semibold'>Question #{currentQuestionNo}</h2>
       <h3 className='text-[1.5rem] font-semibold'>{he.decode(question.question)}</h3>
-      <fieldset className='flex flex-col gap-[1rem]'>
+      <fieldset className='flex flex-col gap-[1rem]' onChange={doesUserAnsweredCorrectly}>
         {
           options.map((option, key)=>{
           return (
             <div key={key} className='flex gap-[0.5rem]'>
-              <input className='w-[3rem] 3-[5rem]' type="radio" name='question' id={he.decode(option)} value={he.decode(option)} />
+              <input  
+              ref = {(el)=> radioRefs.current[key] = el}
+              className='w-[3rem] 3-[5rem]' type="radio" name='question' id={he.decode(option)} value={he.decode(option)} />
               <label htmlFor={he.decode(option)}>{he.decode(option)}</label>
             </div>
           )      
